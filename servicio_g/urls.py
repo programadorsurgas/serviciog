57682f939c0e9c12ee9fe8106f9d6da5d6f4bc22
programadorsurgas <programdorsurgas@hotmail.com>
2017-12-02 10:29:34 -0500

    Se agrega url para cargar los archivos estaicos

--- servicio_g/urls.py
+++ servicio_g/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'^rest/', include(router.urls)),
 ]
 
-if settings.DEBUG:
+if not settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 

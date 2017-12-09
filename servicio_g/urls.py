c807df97efc5d5a6cae02154a99c44571ffd6bfe
programadorsurgas <programdorsurgas@hotmail.com>
2017-12-09 09:55:46 -0500

    Cambiamos los paramtros de autenticación en las urls, adebido a problemas de autenticación. Problema queda en cola para solucionar, por el momento, el servicio funciona bien. También se agrega la url local para pruebas en equipos de desarrollo.

--- servicio_g/urls.py
+++ servicio_g/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.decorators import login_required
 from rest_framework import routers
 from apps.sg_usuario_rest.viewsets import Usr_systemViewSet
 from django.conf import settings
@@ -11,7 +10,7 @@ router.register(r'usuarios', Usr_systemViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^rest/', login_required(include(router.urls))),
+    url(r'^rest/', include(router.urls)),
 ]
 
 if settings.DEBUG:

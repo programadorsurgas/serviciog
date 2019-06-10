e11187e3dcb218c97c4a9a59ff759c9ba56a7abc
programadorsurgas <programadorsurgas@hotmail.com>
2019-06-10 03:27:57 +0000

    servicio para ejecutar en produccion

--- servicio_g/settings.py
+++ servicio_g/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'inp^tj4fw=0cs8a3%o-)gh9+ix43no6u(l8io)!wxp7)@5f7p_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["programadorsurgas.pythonanywhere.com", "127.0.0.1", "192.168.1.9"]
 
@@ -121,8 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "serviciog", "static")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "serviciog", "static")
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

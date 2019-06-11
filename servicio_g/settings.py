24873acbfb3cec3daaa8e2a92060dec9a5e8c0ab
programadorsurgas <programadorsurgas@hotmail.com>
2019-06-10 21:44:01 -0500

    Código con modificación para colocar en producción.
    Lo único que hay que hacer es colocar la variables DEBUG del archivo settings.py en False.

--- servicio_g/settings.py
+++ servicio_g/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'inp^tj4fw=0cs8a3%o-)gh9+ix43no6u(l8io)!wxp7)@5f7p_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["programadorsurgas.pythonanywhere.com", "127.0.0.1", ]
 
@@ -115,10 +115,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+if not DEBUG:
+    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "serviciog", "static")
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "serviciog", "static")
-
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+    MEDIA_URL = '/media/'
